0dca7199e543903ff060a3955a5b677c855a079b
bugraoezdemir <bugraa.ozdemir@gmail.com>
2025-09-22 15:34:11 +0200

    update data_manager.py

--- eubi_bridge/base/data_manager.py
+++ eubi_bridge/base/data_manager.py
@@ -3,7 +3,7 @@ import zarr, natsort
 import shutil, time, os, zarr, psutil, dask, gc, json
 import numpy as np, os, glob, tempfile, importlib
 
-from ome_types.model import OME, Image, Pixels, Channel #TiffData, Plane
+from ome_types.model import OME, Image, Pixels, Channel  # TiffData, Plane
 from ome_types.model import PixelType, Pixels_DimensionOrder, UnitsLength, UnitsTime
 
 from typing import Tuple
@@ -14,14 +14,15 @@ from typing import Union
 
 from eubi_bridge.ngff.multiscales import Pyramid
 from eubi_bridge.ngff.defaults import unit_map, scale_map, default_axes
-from eubi_bridge.utils.convenience import sensitive_glob, is_zarr_group, is_zarr_array, take_filepaths, autocompute_chunk_shape
-from eubi_bridge.base.readers import read_metadata_via_bioio_bioformats, read_metadata_via_extension, read_metadata_via_bfio
+from eubi_bridge.utils.convenience import sensitive_glob, is_zarr_group, is_zarr_array, take_filepaths, \
+    autocompute_chunk_shape
+from eubi_bridge.base.readers import read_metadata_via_bioio_bioformats, read_metadata_via_extension, \
+    read_metadata_via_bfio
 from eubi_bridge.utils.logging_config import get_logger
 from eubi_bridge.ngff.defaults import default_axes, unit_map, scale_map
 
 # Set up logger for this module
 logger = get_logger(__name__)
-
 
 
 def abbreviate_units(measure: str) -> str:
@@ -144,21 +145,22 @@ def expand_units(measure: str) -> str:
     # Return the expanded form if it exists, else return "Unknown"
     return expansions.get(measure.lower(), "Unknown")
 
-def create_ome_xml( # make 5D omexml
-    image_shape: tuple,
-    axis_order: str,
-    pixel_size_x: float = None,
-    pixel_size_y: float = None,
-    pixel_size_z: float = None,
-    pixel_size_t: float = None,
-    unit_x: str = "MICROMETER",
-    unit_y: str = None,
-    unit_z: str = None,
-    unit_t: str = None,
-    dtype: str = "uint8",
-    image_name: str = "Default Image",
-    channel_names: list = None
-    ) -> str:
+
+def create_ome_xml(  # make 5D omexml
+        image_shape: tuple,
+        axis_order: str,
+        pixel_size_x: float = None,
+        pixel_size_y: float = None,
+        pixel_size_z: float = None,
+        pixel_size_t: float = None,
+        unit_x: str = "MICROMETER",
+        unit_y: str = None,
+        unit_z: str = None,
+        unit_t: str = None,
+        dtype: str = "uint8",
+        image_name: str = "Default Image",
+        channel_names: list = None
+) -> str:
     fullaxes = 'xyczt'
     if len(axis_order) != len(image_shape):
         raise ValueError("Length of axis_order must match length of image_shape")
@@ -179,7 +181,6 @@ def create_ome_xml( # make 5D omexml
         else:
             if ax in axis_order.lower():
                 pixel_size_map[f'physical_size_{ax}'] = pixel_size_basemap[f'physical_size_{ax}'] or 1
-
 
     unit_basemap = {
         'time_increment_unit': unit_t,
@@ -225,14 +226,14 @@ def create_ome_xml( # make 5D omexml
             size_map[f'size_{ax}'] = 1
 
     if channel_names is None or len(channel_names) != size_map['size_c']:
-        channels=[Channel(id=f"Channel:{idx}", # TODO: if exists, directly take the channel names
-                          samples_per_pixel=1)
-                  for idx in range(size_map['size_c'])]
+        channels = [Channel(id=f"Channel:{idx}",  # TODO: if exists, directly take the channel names
+                            samples_per_pixel=1)
+                    for idx in range(size_map['size_c'])]
     else:
-        channels=[Channel(id=f"Channel:{idx}", # TODO: if exists, directly take the channel names
-                          samples_per_pixel=1,
-                          name=channel_names[idx])
-                  for idx in range(size_map['size_c'])]
+        channels = [Channel(id=f"Channel:{idx}",  # TODO: if exists, directly take the channel names
+                            samples_per_pixel=1,
+                            name=channel_names[idx])
+                    for idx in range(size_map['size_c'])]
 
     pixels = Pixels(
         dimension_order=Pixels_DimensionOrder(fullaxes.upper()),
@@ -258,10 +259,11 @@ class PFFImageMeta:
         "time_increment", "time_increment_unit",
         "size_x", "size_y", "size_z", "size_t", "size_c"
     }
+
     def __init__(self,
                  path,
                  series,
-                 meta_reader = "bioio"
+                 meta_reader="bioio"
                  ):
         if path.endswith('ome') or path.endswith('xml'):
             from ome_types import OME
@@ -270,16 +272,16 @@ class PFFImageMeta:
             if meta_reader == 'bioio':
                 # Try to read the metadata via bioio
                 try:
-                    omemeta = read_metadata_via_extension(path, series = series)
+                    omemeta = read_metadata_via_extension(path, series=series)
                 except:
                     # If not found, try to read the metadata via bioformats
-                    omemeta = read_metadata_via_bioio_bioformats(path, series = series)
+                    omemeta = read_metadata_via_bioio_bioformats(path, series=series)
             elif meta_reader == 'bfio':
                 try:
-                    omemeta = read_metadata_via_bfio(path) # don't pass series, will be handled afterwards
+                    omemeta = read_metadata_via_bfio(path)  # don't pass series, will be handled afterwards
                 except:
                     # If not found, try to read the metadata via bioformats
-                    omemeta = read_metadata_via_bioio_bioformats(path, series = series)
+                    omemeta = read_metadata_via_bioio_bioformats(path, series=series)
             else:
                 raise ValueError(f"Unsupported metadata reader: {meta_reader}")
         if series is None:
@@ -330,7 +332,7 @@ class PFFImageMeta:
             return None
         ###
         if len(self.pixels.channels) < self.pixels.size_c:
-            chn = ChannelIterator(num_channels = self.pixels.size_c)
+            chn = ChannelIterator(num_channels=self.pixels.size_c)
             channels = chn._channels
         elif len(self.pixels.channels) == self.pixels.size_c:
             channels = []
@@ -339,11 +341,10 @@ class PFFImageMeta:
                 color = expand_hex_shorthand(color)
                 name = channel.name
                 channels.append(dict(
-                    label = name,
-                    color = color
+                    label=name,
+                    color=color
                 ))
         return channels
-
 
 
 class TIFFImageMeta:
@@ -361,7 +362,6 @@ class TIFFImageMeta:
                  meta_reader="bioio",
                  **kwargs
                  ):
-
         if not path.endswith('.tif') and not path.endswith('.tiff'):
             raise Exception(f"The given path does not contain a TIFF file.")
 
@@ -370,7 +370,7 @@ class TIFFImageMeta:
         import tifffile
         tif = tifffile.TiffFile(path)
         self.tiffzarrstore = tif.aszarr()
-        self._zarrdata = zarr.open(self.tiffzarrstore, mode = 'r')
+        self._zarrdata = zarr.open(self.tiffzarrstore, mode='r')
         self._zarrmeta = self.tiffzarrstore._data[series]
         self._meta = tif.series[series]
         self._pff = PFFImageMeta(path, series, meta_reader)
@@ -430,6 +430,7 @@ def expand_hex_shorthand(hex_color):
     expanded = '#' + ''.join([c * 2 for c in shorthand])
     return expanded
 
+
 class NGFFImageMeta:
     def __init__(self,
                  path
@@ -474,18 +475,19 @@ class H5ImageMeta:
 
     def __init__(self,
                  path,
-                 series,
-                 meta_reader="bioio", # placeholder
+                 series=0,
+                 meta_reader="bioio",  # placeholder
                  **kwargs
                  ):
 
         if not path.endswith('.h5'):
             raise Exception(f"The given path does not contain a TIFF file.")
-
+        if series is None:
+            series = 0
         self.path = path
         import h5py
         f = h5py.File(path)
-        dset_name = list(f.keys())[0]
+        dset_name = list(f.keys())[series]
         ds = f[dset_name]
         self._ds = ds
         self._attrs = dict(ds.attrs)
@@ -497,7 +499,6 @@ class H5ImageMeta:
         # self.omemeta.images[0].pixels = self.pixels
         # self.pyr = self._pff.pyr
         self.pyr = None
-
 
     def get_axes(self):
         attrs = self._attrs
@@ -572,6 +573,7 @@ class H5ImageMeta:
     def get_channels(self):
         return []
 
+
 # path = f"/home/oezdemir/PycharmProjects/TIM2025/data/h5/ilastik_img/Patient1_001.h5"
 # h5 = H5ImageMeta(path, series = 0)
 # h5.get_axes()
@@ -592,6 +594,7 @@ def expand_hex_shorthand(hex_color):
 
     expanded = '#' + ''.join([c * 2 for c in shorthand])
     return expanded
+
 
 class NGFFImageMeta:
     def __init__(self,
@@ -634,11 +637,12 @@ class ArrayManager:
         "time_increment", "time_increment_unit",
         "size_x", "size_y", "size_z", "size_t", "size_c"
     }
+
     def __init__(self,
                  path: Union[str, Path] = None,
                  series: int = None,
                  metadata_reader='bfio',  # bfio or aicsimageio
-                 skip_dask = False,
+                 skip_dask=False,
                  **kwargs
                  ):
         self.path = path
@@ -709,10 +713,10 @@ class ArrayManager:
         return self
 
     def get_pixel_size_basemap(self,
-                               t = 1,
-                               z = 1,
-                               y = 1,
-                               x = 1,
+                               t=1,
+                               z=1,
+                               y=1,
+                               x=1,
                                **kwargs
                                ):
         return {
@@ -723,10 +727,10 @@ class ArrayManager:
         }
 
     def get_unit_basemap(self,
-                         t = 'second',
-                         z = 'micrometer',
-                         y = 'micrometer',
-                         x = 'micrometer',
+                         t='second',
+                         z='micrometer',
+                         y='micrometer',
+                         x='micrometer',
                          **kwargs
                          ):
         return {
@@ -737,8 +741,8 @@ class ArrayManager:
         }
 
     def update_meta(self,
-                    new_scaledict = {},
-                    new_unitdict = {},
+                    new_scaledict={},
+                    new_unitdict={},
                     # new_channels = [],
                     ):
 
@@ -762,10 +766,10 @@ class ArrayManager:
         else:
             units = [expand_units(unitdict[ax]) for ax in self.caxes]
 
-        self.set_arraydata(array = self.array,
-                           axes = self.axes,
-                           units = units,
-                           scales = scales)
+        self.set_arraydata(array=self.array,
+                           axes=self.axes,
+                           units=units,
+                           scales=scales)
 
     def _ensure_correct_channels(self):
         if self.array is None:
@@ -791,12 +795,12 @@ class ArrayManager:
             self.channels[i] = channel
 
     def set_arraydata(self,
-                      array = None,
-                      axes = None,
-                      units = None,
-                      scales = None,
-                      channels = None,
-                      **kwargs # placehold
+                      array=None,
+                      axes=None,
+                      units=None,
+                      scales=None,
+                      channels=None,
+                      **kwargs  # placehold
                       ):
 
         axes = axes or self.img.get_axes()
@@ -856,7 +860,7 @@ class ArrayManager:
             return [self.unitdict[ax] for ax in self.axes]
         else:
             raise ValueError
-    
+
     @property
     def channels(self):
         if self._channels is not None:
@@ -868,8 +872,8 @@ class ArrayManager:
         return [self.chunkdict[ax] for ax in self.axes]
 
     def sync_pyramid(self,
-                     create_omexml_if_not_exists = False
-                     ): ### TODO: reconsider for improvement
+                     create_omexml_if_not_exists=False
+                     ):  ### TODO: reconsider for improvement
         ### This is only to be used to update pyramidal metadata in place.
         """
         Synchronizes the scale and unit metadata with the Pyramid (if a Pyramid exists).
@@ -881,10 +885,10 @@ class ArrayManager:
             raise Exception(f"No pyramid exists.")
 
         self.pyr.update_scales(**self.scaledict,
-                                     # hard=True
+                               # hard=True
                                )
         self.pyr.update_units(**self.unitdict,
-                                   # hard=True
+                              # hard=True
                               )
         if self.omemeta is None:
             self.omemeta = create_ome_xml(
@@ -918,12 +922,12 @@ class ArrayManager:
         unit_basemap = self.get_unit_basemap(
             **self.unitdict
         )
-        self.omemeta = create_ome_xml(image_shape = self.array.shape,
-                                      axis_order = self.axes,
+        self.omemeta = create_ome_xml(image_shape=self.array.shape,
+                                      axis_order=self.axes,
                                       **pixel_size_basemap,
                                       **unit_basemap,
-                                      dtype = str(self.array.dtype),
-                                      channel_names = [channel['label'] for channel in self.channels]
+                                      dtype=str(self.array.dtype),
+                                      channel_names=[channel['label'] for channel in self.channels]
                                       )
         self.pixels = self.omemeta.images[0].pixels
         missing_fields = self.essential_omexml_fields - self.pixels.model_fields_set
@@ -933,11 +937,11 @@ class ArrayManager:
 
     def save_omexml(self,
                     base_path: str,
-                    overwrite = False
+                    overwrite=False
                     ):
         assert self.omemeta is not None, f"No ome-xml exists."
         gr = zarr.group(base_path)
-        gr.create_group('OME', overwrite = overwrite)
+        gr.create_group('OME', overwrite=overwrite)
 
         path = os.path.join(gr.store.root, 'OME/METADATA.ome.xml')
 
@@ -946,9 +950,9 @@ class ArrayManager:
 
         if gr.info._zarr_format == 2:
             gr['OME'].attrs["series"] = [self._seriesattrs]
-        else: # zarr format 3
-            gr['OME'].attrs["ome"] = dict(version = "0.5",
-                                          series = [str(self._seriesattrs)]
+        else:  # zarr format 3
+            gr['OME'].attrs["ome"] = dict(version="0.5",
+                                          series=[str(self._seriesattrs)]
                                           )
 
     def squeeze(self):
@@ -979,11 +983,11 @@ class ArrayManager:
         self.set_arraydata(newarray, newaxes, newunits, newscales)
 
     def crop(self,
-             trange = None,
-             crange = None,
-             zrange = None,
-             yrange = None,
-             xrange = None,
+             trange=None,
+             crange=None,
+             zrange=None,
+             yrange=None,
+             xrange=None,
              ):
         slicedict = {
             't': slice(*trange) if trange is not None else slice(None),
@@ -992,7 +996,7 @@ class ArrayManager:
             'y': slice(*yrange) if yrange is not None else slice(None),
             'x': slice(*xrange) if xrange is not None else slice(None),
         }
-        slicedict = {ax: r for ax,r in slicedict.items() if ax in self.axes}
+        slicedict = {ax: r for ax, r in slicedict.items() if ax in self.axes}
         slices = tuple([slicedict[ax] for ax in self.axes])
         array = self.array[slices]
         self.set_arraydata(array, self.axes, self.units, self.scales)
@@ -1005,20 +1009,19 @@ class ArrayManager:
         array = self.array.map_blocks(cupy.asarray)
         self.set_arraydata(array, self.axes, self.units, self.scales)
 
-    def split(self): ###TODO
+    def split(self):  ###TODO
         pass
 
     def get_autocomputed_chunks(self,
-                                dtype = None
+                                dtype=None
                                 ):
         array_shape = self.array.shape
         dtype = dtype or self.array.dtype
         axes = self.axes
-        chunk_shape = autocompute_chunk_shape(array_shape = array_shape,
-                                              axes = axes,
-                                              dtype = dtype)
+        chunk_shape = autocompute_chunk_shape(array_shape=array_shape,
+                                              axes=axes,
+                                              dtype=dtype)
         return chunk_shape
-
 
 
 class ChannelIterator:
